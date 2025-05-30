f729d6fb86d3bd10adb4bdd4b49689c25273b74b
Nguyen Phong <ittk54k@gmail.com>
2025-05-30 15:59:40 +0700

    variable

--- index2/index2.js
+++ index2/index2.js
@@ -1,2 +1,9 @@
 console.log("hello word form javascript");
 console.log("abcd");
+console.log('xyz');
+let a = 10;
+let b = 8.9;
+console.log(a + b, a > b, a < b);
+let x = 10 + " GPA";// can use "...", '...', `...`.
+console.log(x);
+console.log("Test String", 'x = ', x,'a =', a,'b = ', b,'And a + b = ',a+b);
