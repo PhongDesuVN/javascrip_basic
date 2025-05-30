93fff4103d68ec6df74f1974794d8bee12dc759e
Nguyen Phong <ittk54k@gmail.com>
2025-05-30 17:09:53 +0700

    variable

--- index2/index2.js
+++ index2/index2.js
@@ -7,3 +7,8 @@ console.log(a + b, a > b, a < b);
 let x = 10 + " GPA";// can use "...", '...', `...`.
 console.log(x);
 console.log("Test String", 'x = ', x,'a =', a,'b = ', b,'And a + b = ',a+b);
+
+let c = 'Tôi là String - chuỗi'
+console.log(c);
+console.log(4 + 5 + 'a'); // cái này là 9 + a = 9a
+console.log('a' + 5 + 4); // cái này là a5 + 4 = a45
