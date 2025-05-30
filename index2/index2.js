df35cd779365d49e231f121572360a45e0df5a71
Nguyen Phong <ittk54k@gmail.com>
2025-05-30 17:22:34 +0700

    variable

--- index2/index2.js
+++ index2/index2.js
@@ -12,3 +12,7 @@ let c = 'Tôi là String - chuỗi'
 console.log(c);
 console.log(4 + 5 + 'a'); // cái này là 9 + a = 9a
 console.log('a' + 5 + 4); // cái này là a5 + 4 = a45
+let text =`I'm form "Hai Duong"
+// Nếu muốn xuống dòng cứ space bình thương không cần thêm gì
+Tôi đến từ Hải Dương`;// Lưu ý dùng dấu `...` khi bên trong có dấu '...' or "..." 
+console.log(text);
